35f8e541881b4863f867c276bb90c290ffb1f7d8
nicolascou <nicolascousillas1@gmail.com>
2022-05-25 11:36:27 +0200

    Version 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,32 @@
+import  React from 'react';
+import renderer from 'react-test-renderer'
+import { ProductCard, ProductImage } from '../../src/components';
+import { product2 } from '../data/products';
+
+describe('ProductImage', () => { 
+
+    test('should layout component correctly', () => {
+        
+        const wrapper = renderer.create(
+            <ProductImage img='https://hola.jpg' />
+        )
+        expect( wrapper.toJSON() ).toMatchSnapshot();
+        
+    })
+
+    test('should show component with product image', () => { 
+
+        const wrapper = renderer.create(
+            <ProductCard product={ product2 }>
+                {
+                    () => (
+                        <ProductImage />
+                    )
+                }
+            </ProductCard>
+        )
+        expect( wrapper.toJSON() ).toMatchSnapshot();
+        
+    })
+
+})
---
+++ test/components/ProductTitle.test.tsx
@@ -0,0 +1,32 @@
+import  React from 'react';
+import renderer from 'react-test-renderer'
+import { ProductCard, ProductTitle } from '../../src/components';
+import { product1 } from '../data/products';
+
+describe('ProductTitle', () => { 
+
+    test('should layout component correctly', () => {
+        
+        const wrapper = renderer.create(
+            <ProductTitle title='Custom Product' className='custom-class' />
+        )
+        expect( wrapper.toJSON() ).toMatchSnapshot();
+        
+    })
+
+    test('should show component with product name', () => { 
+
+        const wrapper = renderer.create(
+            <ProductCard product={ product1 }>
+                {
+                    () => (
+                        <ProductTitle />
+                    )
+                }
+            </ProductCard>
+        )
+        expect( wrapper.toJSON() ).toMatchSnapshot();
+        
+    })
+
+})
